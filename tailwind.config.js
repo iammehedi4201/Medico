2ac287bc95efba789583d5bdca3b483be28b7f54
Mehedi Hasan <mehedi15-13298@diu.edu.bd>
2023-08-22 00:28:47 +0600

    modified the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    plugins: [require("daisyui")],
+    extend: {},
   },
-  
-}
+  plugins: [require("daisyui")],
+};
