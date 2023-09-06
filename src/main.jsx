c99a2e9d9b99cffc7e912435abf1d395e5a6bc99
Mehedi Hasan <mehedi15-13298@diu.edu.bd>
2023-09-07 00:47:31 +0600

    modified

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes.jsx'
 import { Toaster } from 'react-hot-toast'
+import "aos/dist/aos.css"; // Import AOS styles
+import AOS from "aos"; // Import AOS library
+
+AOS.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
